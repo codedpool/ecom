22e41d36dcb47e5feb6fa9165b4bf230e41fd59a
Anamika <anamikasingh200205@gmail.com>
2025-02-12 00:20:57 +0530

    Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,23 +2,26 @@ import React from 'react';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UserLayout from './components/Layout/UserLayout';
-import ProductDetails from './components/Products/ProductDetails'; // Added from stashed changes
+
 import { toast, Toaster } from 'sonner';
 import Home from './pages/Home';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Toaster position="top-right"/>
-    <Routes>
-      <Route path='/' element={<UserLayout/>}>{/* User LAyout*/}</Route>
-      <Route>{/* Admin Layout*/}</Route>
-      <Route path="login" element={<Login/>}/>
-      <Route/>
-      
-     
-      
-    </Routes>
+      <Toaster position="top-right" />
+      <Routes>
+        {/* User Layout Routes */}
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} /> {/* Home page */}
+          {/* Add more user routes here if needed */}
+        </Route>
+
+        {/* Admin Layout Routes (Placeholder) */}
+        <Route>
+          {/* Admin routes will go here */}
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 };
