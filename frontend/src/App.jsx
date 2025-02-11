0d8c6ffe517237e460f81e77e6f702b589b4534f
Anamika <anamikasingh200205@gmail.com>
2025-02-12 00:13:27 +0530

    Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import UserLayout from './components/Layout/UserLayout'
 
 import {toast,Toaster} from "sonner";
-import Home from './pages/Home';
+import Login from './pages/Login';
+
 
 
 
@@ -15,9 +16,10 @@ const App = () => {
     <Toaster position="top-right"/>
     <Routes>
       <Route path='/' element={<UserLayout/>}>{/* User LAyout*/}</Route>
-  
-      
       <Route>{/* Admin Layout*/}</Route>
+      <Route path="login" element={<Login/>}/>
+      <Route/>
+      
      
       
     </Routes>
